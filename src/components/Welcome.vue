8d9a1fb67a2ba80cb7f50652cd0e09aaee72c4f5
Daniel Barion <danielbarionn@gmail.com>
2018-08-15 16:41:39 -0300

    add navbar

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,82 +1,139 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <blockquote class="main">
-          <h2>Welcome Adventurer</h2>
-          <v-form v-model="valid" ref="form">
-            <v-text-field
-              v-model="name"
-              label="What are your name ? "
-              :rules="nameRules"
-              :counter="30"
-              required
-            ></v-text-field>
-            <v-btn
-              block
-              color="info"
-              :disabled="!valid"
-              @click="submit"
-            >Next</v-btn>
-          </v-form>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+  <v-container fluid class="main-container">
+    <v-layout>
+      <nav class="navbar">
+        <ul>
+          <!-- <li><i class="material-icons">home</i></li> -->
+          <li><img src="../assets/shield.svg" alt="Shield"></li>
+          <li><a href="javascript:;">SKILLS</a></li>
+          <li><a href="javascript:;">PROJECTS</a></li>
+          <li><a href="javascript:;">CONTACT</a></li>
+        </ul>
+      </nav>
+      <header class="header">
+        <div class="welcome-msg">
+          <div class="left">
+            <!-- <h1>Welcome to my Space</h1> -->
+            <h1>Lorem ipsum dolor</h1>
+            <span>lorem ipsum dolor sit amet</span>
+          </div>
+          <div class="right">
+            <!-- <h1>Welcome to my Space</h1> -->
+            <h1>Lorem ipsum dolor</h1>
+            <span>lorem ipsum dolor sit amet</span>
+          </div>
+        </div>
+      </header>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import store from '../vuex/store'
+import store from '@/vuex/store'
 
 export default {
   props: {
     msg: String
   },
   data: () => ({
-    valid: false,
-    name: null,
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => /^[a-zA-Z]+$/.test(v) || 'No special character allowed',
-      v => (v && v.length <= 30) || 'Name must be less than 30 characters'
-    ],
+
   }),
-  methods: {
-    submit () {
-      if (this.$refs.form.validate()) {
-        store.commit('addMe', {
-          id: 1,
-          name: this.name,
-          hp: 100,
-          mp: 100,
-          atk: 1,
-          atkspd: 1,
-          atkbar: 0
-        })
-        this.$router.push('Race')
-      }
-    },
-  }
+  methods: {}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h2 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+.main-container {
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.header {
+  height: 800px;
+  width: 100vw;
+  background-image: url('../assets/shield.jpg');
+  /* background-image: url('../assets/header.jpg'); */
+  background-size: cover;
+  background-position: 0;
+  padding: 0;
 }
-a {
-  color: #42b983;
+.welcome-msg {
+  display: flex;
+  color: aliceblue;
+  font-family: 'Ubuntu', sans-serif;
+  height: 100%;
+  justify-content: space-between;
 }
-.main {
-  width: 20rem;
+.welcome-msg .left {
+  padding-top: 15rem;
+  padding-left: 10rem;
+}
+.welcome-msg .right {
+  display: flex;
+  align-self: flex-end;
+  flex-direction: column;
+  padding-bottom: 16rem;
+  padding-right: 10rem;
+}
+.welcome-msg .right span{
+  align-self: flex-end;
+}
+.welcome-msg h1 {
+  font-size: 56px;
+  text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
+              0px 8px 13px rgba(0,0,0,0.1),
+              0px 18px 23px rgba(0,0,0,0.1);
+}
+.welcome-msg span {
+  font-size: 24px;
+  text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
+              0px 8px 13px rgba(0,0,0,0.1),
+              0px 18px 23px rgba(0,0,0,0.1);
+}
+
+.navbar {
+  position: absolute;
+  color: aliceblue;
+  width: 100%;
+  font-size: 22px;
+}
+.navbar ul {
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  list-style: none;
+  right: 5rem;
+  top: 1rem;
+}
+.navbar li {
+  padding: 0.5rem;
+}
+.navbar li i {
+  font-size: 32px;
+}
+.navbar li img {
+  height: 32px;
+  width: 32px;
+}
+.navbar li a {
+  text-decoration: none;
+  color: aliceblue;
+  /* background-image: linear-gradient(141deg, red 0%, white 50%, #2cb5e8 75%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent; */
+
+  text-shadow: 0px 4px 3px rgba(0,0,0,0.4);
+
+  -o-transition: .5s;
+  -ms-transition: .5s;
+  -moz-transition: .5s;
+  -webkit-transition: .5s;
+  transition: .5s all;
+}
+.navbar li a:hover {
+  background-image: linear-gradient(141deg, red 0%, white 50%, #2cb5e8 75%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
-import Race from '@/components/Race'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
-    },
-    {
-      path: '/Race',
-      name: 'Race',
-      component: Race
     }
   ]
 })
