4fbb18539eb75a9a709e2cc9bd2483839a4ab6fe
Daniel Barion <danielbarionn@gmail.com>
2018-08-22 09:01:29 -0300

    Update

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'web/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
     /**
